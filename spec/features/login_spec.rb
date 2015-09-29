fa20df5cf5226ee83094c7a811deb22a7a404d92
Chris DiMartino <vwchris@gmail.com>
2015-09-29 12:17:40 -0400

    Adding more login/logout feature tests

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,19 +1,34 @@
 require 'rails_helper'
 
-describe "the signin process" do
-  before :each do
-    create(:user)
-  end
-
+describe "the login/logout process" do
+  let(:login_page) { LoginPageHelper.new }
   let(:user) { attributes_for(:user) }
 
-  it "signs me in" do
-    visit root_url
-    within("#login_form") do
-      fill_in 'session_username', :with => user[:username]
-      fill_in 'session_password', :with => user[:password]
+  before :each do
+    create(:user)
+    login_page.visit_page.login(user)
+  end
+
+  describe 'when logged out' do
+    it "signs me in" do
+      expect(page).to have_content "Welcome #{user[:username]}"
     end
-    click_button 'Sign In'
-    expect(page).to have_content "Welcome #{user[:username]}"
+  end
+
+  describe 'when logged in' do
+    it 'has a log out link' do
+      expect(page).to have_link 'Logout'
+    end
+
+    it 'logs me out' do
+      click_link 'Logout'
+      expect(page).to have_content 'Logged Out'
+    end
+
+    it 'has log in fields' do
+      click_link 'Logout'
+      expect(page).to have_field 'session_username'
+      expect(page).to have_field 'session_password'
+    end
   end
 end
---
+++ spec/support/page_helpers/login_page_helper.rb
@@ -0,0 +1,16 @@
+class LoginPageHelper
+  include Capybara::DSL
+
+  def visit_page
+    visit '/'
+    self
+  end
+
+  def login user
+    within("#login_form") do
+      fill_in 'session_username', :with => user[:username]
+      fill_in 'session_password', :with => user[:password]
+    end
+    click_button 'Sign In'
+  end
+end
